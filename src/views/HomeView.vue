02adea0bf90b3c2115a52a6518275d97ed4bbfa9
pangpt <tridarmapanggih@gmail.com>
2022-07-17 20:54:25 +0800

    upload projek vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderLocal />
+    <HeroLocal />
+    <OutterLocal />
+    <InstaLocal />
+    <PartnerLocal />
+    <FooterLocal />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderLocal from '@/components/HeaderLocal.vue';
+import HeroLocal from '@/components/HeroLocal.vue';
+import OutterLocal from '@/components/OutterLocal.vue';
+import InstaLocal from '@/components/InstaLocal.vue';
+import PartnerLocal from '@/components/PartnerLocal.vue';
+import FooterLocal from '@/components/FooterLocal.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderLocal,
+    HeroLocal,
+    OutterLocal,
+    InstaLocal,
+    PartnerLocal,
+    FooterLocal,
+  },
+};
+</script>
